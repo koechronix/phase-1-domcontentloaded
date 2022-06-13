86d02e23bd253bae1efecc71ea8d250a4a29498f
Amos Koech <koechronix@yahoo.com>
2022-06-13 12:40:47 -0400

    commit tree f7eb1cbe837ed046c399c4a3dc90f38b22f23bb7

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",
+function(){
+    let changes = document.getElementById("text")
+    changes.textContent = "This is really cool!"
+}
+
+)
